fded1d9f4455b0af8b7d03c444e9b9632d61f794
Kanwal Arora <kanwalarora029@gmail.com>
2023-06-23 20:37:51 +0100

    Predict Pipeline and Data Ingestion

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import sys
 from sensor.exception import CustomException
+from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.logger import logging
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
 
 
 if __name__=='__main__':
-    pass
+    training_pipeline_config = TrainingPipelineConfig()
+    data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    print(data_ingestion_config.__dict__)
